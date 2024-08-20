190dc4e2b128fcd7201caeab4ea37cd0d663e8e6
filipwojdak <f.wojdak@gmail.com>
2024-08-20 19:29:29 +0200

    fix models

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,7 @@ export class AppService {
 
   getHouseModels(): HouseModel[] {
     const data = fs.readFileSync(
-      path.join(this.dataPath, 'models.data'),
+      path.join(this.dataPath, 'models.json'),
       'utf-8',
     );
     return JSON.parse(data);
