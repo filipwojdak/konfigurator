e17d1fe91080b7fdd4cb8f593542547248c949b4
filipwojdak <f.wojdak@gmail.com>
2024-08-22 15:19:53 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['PATCH', 'GET', 'POST'],
+  });
   await app.listen(8000);
 }
 bootstrap();
